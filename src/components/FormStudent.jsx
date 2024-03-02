96cd8247889cc8f0be8236e64dddf57d6da7300f
widmaker666 <alexandre.hontcharouk69@gmail.com>
2024-03-02 10:45:17 +0100

    created new student works !

--- src/components/FormStudent.jsx
+++ src/components/FormStudent.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function ImageUploader({ onImageChange }) {
   const handleImageChange = (event) => {
@@ -16,44 +17,81 @@ function ImageUploader({ onImageChange }) {
 
 function FormStudent() {
   const [avatar, setAvatar] = useState("");
+  const [values, setValues] = useState({
+    photo: "",
+    firstName: "",
+    lastName: "",
+    dateOfBirth: "",
+    gender: "",
+  });
 
   const handleAvatarChange = (image) => {
-    setAvatar(URL.createObjectURL(image));
+    const imageUrl = URL.createObjectURL(image);
+    setAvatar(imageUrl);
+    setValues(() => setValues({ ...values, photo: imageUrl }));
+  };
+
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post("http://localhost:3001/students", values)
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
     <>
-      <form className="form-add-student">
-        <div className="img">
-          {avatar && (
-            <img
-              src={avatar}
-              alt="Avatar"              
-            />
-          )}
-        </div>
-        <div className="div-img">          
+      <form onSubmit={handleSubmit} className="form-add-student">
+        <div className="img">{avatar && <img src={avatar} alt="Avatar" />}</div>
+        <div className="div-img">
           <ImageUploader onImageChange={handleAvatarChange} />
         </div>
 
         <div className="div-fName">
           <label htmlFor="firstName">First Name</label>
-          <input type="text" id="firstName" placeholder="Freddy...." />
+          <input
+            type="text"
+            id="firstName"
+            onChange={(e) =>
+              setValues({ ...values, firstName: e.target.value })
+            }
+            placeholder="Freddy...."
+          />
         </div>
 
         <div className="div-lName">
           <label htmlFor="lastName">First Name</label>
-          <input type="text" id="lastName" placeholder="Krueger...." />
+          <input
+            type="text"
+            id="lastName"
+            onChange={(e) => setValues({ ...values, lastName: e.target.value })}
+            placeholder="Krueger...."
+          />
         </div>
 
         <div className="div-birth">
           <label htmlFor="date-birth">Date of Birth</label>
-          <input type="date" id="date-birth" />
+          <input
+            type="date"
+            onChange={(e) =>
+              setValues({ ...values, dateOfBirth: e.target.value })
+            }
+            id="date-birth"
+          />
         </div>
 
         <div className="div-gender">
           <label htmlFor="gender">Gender</label>
-          <select name="gender" id="gender">
+          <select
+            name="gender"
+            id="gender"
+            onChange={(e) => setValues({ ...values, gender: e.target.value })}
+          >
             <option value="male">Male</option>
             <option value="female">Female</option>
           </select>
@@ -61,7 +99,9 @@ function FormStudent() {
 
         <button type="submit">Confirmed</button>
       </form>
-      <Link className="back-home" to="/">Back</Link>
+      <Link className="back-home" to="/">
+        Back
+      </Link>
     </>
   );
 }
